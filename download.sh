bfe76e5388473a61e9ab0568385dd64a26281a7a
Daniele Santini <daniele.santini-external@vivaticket.com>
2025-11-26 17:35:47 +0100

    WIP november update

--- download.sh
+++ download.sh
@@ -10,26 +10,28 @@ ZIP_DIR_PATH="$(dirname "$0")/zip"
 
 mkdir -p "$ZIP_DIR_PATH"
 
-while IFS=$'\t' read -r file_name region province wmit_url igm_url igm_date ; do
-    if [[ "$file_name" == "File" ]]; then
-        # Skip header line
+while IFS=$'\t' read -r region province file_name wmit_url igm_url igm_date latest ; do
+    if [[ "$file_name" == "File" || "$latest" != "yes" ]]; then
+        # Skip header line and old files
+        #echo "===> $region/$province/$igm_date: SKIPPED"
         continue
     fi
 
     # ${var,,} makes the value lowercase, used for case insensitive comparison
     if [[ -n "$AREA_NAME" && "${province,,}" != "${AREA_NAME,,}" && "${region,,}" != "${AREA_NAME,,}" && "${file_name:0:2}" != "${AREA_NAME^^}" ]]; then
-        #echo "===> $region / $province: SKIPPED"
+        #echo "===> $region/$province/$igm_date: SKIPPED"
         continue
     fi
 
     file_path="$ZIP_DIR_PATH/$file_name"
     if [[ -f "$file_path" ]]; then
-        echo "===> $region / $province: Already downloaded in '$file_path'"
+        echo "===> $region/$province/$igm_date: Already downloaded in '$file_path'"
     else
-        echo "===> $region / $province: Downloading from $wmit_url"
-        curl --fail --output "$file_path" "$wmit_url" && \
-            echo "===> $region / $province: Download of '$file_path' COMPLETED" || \
-            echo "===> $region / $province: Download of '$file_path' FAILED"
+        [[ "$wmit_url" == "TODO" ]] && url="$igm_url" || url="$wmit_url"
+        echo "===> $region/$province/$igm_date: Downloading from $url"
+        curl --fail --output "$file_path" "$url" && \
+            echo "===> $region/$province/$igm_date: Download of '$file_path' COMPLETED" || \
+            echo "===> $region/$province/$igm_date: Download of '$file_path' FAILED"
     fi
 done < ./dbsn.tsv
 
--- filter.sh
+++ filter.sh
@@ -34,29 +34,30 @@ TEMP_DIR_PATH="$(dirname "$0")/data/$OUT_NAME"
 mkdir -p "$TEMP_DIR_PATH"
 mkdir -p "$UNZIPPED_DIR_PATH"
 
-while IFS=$'\t' read -r file_name region province wmit_url igm_url igm_date ; do
-    if [[ "$file_name" == "File" ]]; then
-        # Skip header line
+while IFS=$'\t' read -r region province file_name wmit_url igm_url igm_date latest ; do
+    if [[ "$file_name" == "File" || "$latest" != "yes" ]]; then
+        # Skip header line and old files
+        #echo "===> $region/$province/$igm_date: SKIPPED"
         continue
     fi
     
     # ${var,,} makes the value lowercase, used for case insensitive comparison
     if [[ -n "$AREA_NAME" && "${province,,}" != "${AREA_NAME,,}" && "${region,,}" != "${AREA_NAME,,}" && "${file_name:0:2}" != "${AREA_NAME^^}" ]]; then
-        #echo "===> $region - $province: SKIPPED"
+        #echo "===> $region/$province/$igm_date: SKIPPED"
         continue
     fi
 
     file_name_no_extension="${file_name%.zip}"
     province_file_path="$TEMP_DIR_PATH/$file_name_no_extension.$OUT_EXTENSION"
     if [ -f "$province_file_path" ]; then
-        echo "===> $region - $province: Already extracted and filtered in '$province_file_path'"
+        echo "===> $region/$province/$igm_date: Already extracted and filtered in '$province_file_path'"
         continue
     fi
     
     province_zip_path="$ZIP_DIR_PATH/$file_name"
     unzipped_dir_path="$UNZIPPED_DIR_PATH/$file_name"
     if [ -e "$unzipped_dir_path" ]; then
-        echo "===> $region - $province: Already extracted in '$unzipped_dir_path'"
+        echo "===> $region/$province/$igm_date: Already extracted in '$unzipped_dir_path'"
     else
         echo "===> Extraction of $province_zip_path in $unzipped_dir_path"
         unzip "$province_zip_path" -d "$unzipped_dir_path"
@@ -67,12 +68,12 @@ while IFS=$'\t' read -r file_name region province wmit_url igm_url igm_date ; do
     echo "===> Filtering of '$gdb_dir_path' in '$province_file_path'"
     if [[ -z "$GDAL_FILTER" ]]; then
         ogr2ogr -f "$OUT_DRIVER" -t_srs 'EPSG:4326' -nln "$OUT_NAME" -skipfailures "$province_file_path" "$gdb_dir_path" "$GDAL_LAYER" \
-        && echo "===> $region - $province: Filtering COMPLETED" \
-        || (echo "===> !!!!!!!!!! $region - $province: Filtering FAILED !!!!!!!!!!" && rm "$province_file_path");
+        && echo "===> $region/$province/$igm_date: Filtering COMPLETED" \
+        || (echo "===> !!!!!!!!!! $region/$province/$igm_date: Filtering FAILED !!!!!!!!!!" && rm "$province_file_path");
     else
         ogr2ogr -f "$OUT_DRIVER" -t_srs 'EPSG:4326' -nln "$OUT_NAME" -skipfailures -where "$GDAL_FILTER" "$province_file_path" "$gdb_dir_path" "$GDAL_LAYER"\
-        && echo "===> $region - $province: Filtering COMPLETED" \
-        || (echo "===> !!!!!!!!!! $region - $province: Filtering FAILED !!!!!!!!!!" && rm "$province_file_path");
+        && echo "===> $region/$province/$igm_date: Filtering COMPLETED" \
+        || (echo "===> !!!!!!!!!! $region/$province/$igm_date: Filtering FAILED !!!!!!!!!!" && rm "$province_file_path");
     fi
 done < ./dbsn.tsv
 
