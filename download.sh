52c4acc01f73abcbe08c46760e0f363511a4b85f
giopera <perigio735@gmail.com>
2026-01-03 18:11:33 +0100

    Expanded download utility commands

--- download.sh
+++ download.sh
@@ -1,25 +1,46 @@
 #!/bin/bash
 set -e
 
-# Usage: ./download.sh [<nome_regione_o_provincia>]
+PREF_WM=0
+AREA_SET=1
 
-AREA_NAME="$1"
+while getopts "wihad:" flag; do
+    case $flag in
+        w)
+            PREF_WM=1
+            ;;
+        i)
+            PREF_WM=-1
+            ;;
+        a)
+            AREA_SET=0
+            ;;
+        d)
+            DATE=$OPTARG
+            ;;
+        *) echo -e "\nDBSN Downloader\n\nFlags:\n-w\t\tPrefer WikiMedia downloads\n-i\t\tPrefer IGM downloads\n-a \t\tDownload every file available\n-d <date>\tDownload datasets of specified date in format YYYY-MM-DD\n" && exit;;
+    esac
+done
 
 ZIP_DIR_PATH="$(dirname "$0")/zip"
+shift $((OPTIND - 1))
 
+if [[ $1 && $AREA_SET != 0 ]]; then 
+    AREA_NAME=$1
+fi
 
 mkdir -p "$ZIP_DIR_PATH"
 
 while IFS=$'\t' read -r region province file_name wmit_url igm_url igm_date latest ; do
-    if [[ "$file_name" == "File" || "$latest" != "yes" ]]; then
+    if [[ "$file_name" == "File" || (-z "$DATE" && $latest != "yes") || ( "$DATE" && "$DATE" != $igm_date) ]]; then
         # Skip header line and old files
-        #echo "===> $region/$province/$igm_date: SKIPPED"
+        echo "===> $region/$province/$igm_date: SKIPPED"
         continue
     fi
 
     # ${var,,} makes the value lowercase, used for case insensitive comparison
     if [[ -n "$AREA_NAME" && "${province,,}" != "${AREA_NAME,,}" && "${region,,}" != "${AREA_NAME,,}" && "${file_name:0:2}" != "${AREA_NAME^^}" ]]; then
-        #echo "===> $region/$province/$igm_date: SKIPPED"
+        echo "===> $region/$province/$igm_date: SKIPPED"
         continue
     fi
 
@@ -27,7 +48,14 @@ while IFS=$'\t' read -r region province file_name wmit_url igm_url igm_date late
     if [[ -f "$file_path" ]]; then
         echo "===> $region/$province/$igm_date: Already downloaded in '$file_path'"
     else
-        [[ "$wmit_url" == "TODO" ]] && url="$igm_url" || url="$wmit_url"
+        if [[ $PREF_WM == 1 ]]; then
+            [[ "$wmit_url" == "TODO" ]] && echo "Wikimedia download not available" || url="$wmit_url"
+        elif [[ $PREF_WM == -1 ]]; then
+            url="$igm_url"
+        elif [[ $PREF_WM = 0 ]]; then
+            [[ "$wmit_url" == "TODO" ]] && url="$igm_url" || url="$wmit_url"
+        fi
+            
         echo "===> $region/$province/$igm_date: Downloading from $url"
         curl --fail --output "$file_path" "$url" && \
             echo "===> $region/$province/$igm_date: Download of '$file_path' COMPLETED" || \
